d684ca0e414cc3f8963f7b7c5afe629c00d37457
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-05-14 21:22:53 +0530

    initial commit

---
+++ typecasting.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//typecasting syntax
+//(type)value;
+int main(){
+	int a=3;
+	float b=(float)54/5;
+	
+	printf("the value of a is %d\n",a);
+	printf("the value of a is %d\n",b);
+	printf("the value of a is %f\n",b);
+		printf("the value of a is %d\n",(int)b);
+		
+	return 0;
+}
