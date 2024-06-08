fa417f04b08f94f5d4e73a697badb5605bc0820b
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-06-09 01:56:13 +0530

    initial commit

--- arrayimplementationstack.c
+++ arrayimplementationstack.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
-//push operation
-#include <stdlib.h>
+#include<stdlib.h>
 #define MAX 4
 int stack_arr[MAX];
 int top = -1;
 
 void push(int data)
 {
-    if (top == MAX - 1)
-    {
+    if(top==MAX-1){
         printf("stack overflow\n");
         return;
     }
     top = top + 1;
     stack_arr[top] = data;
-}
-int main()
+}int main()
 {
     push(1);
     push(2);
     push(3);
     push(4);
-    //push(5); stack overflow
+    
     return 0;
 }
